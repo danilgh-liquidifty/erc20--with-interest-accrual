bf98885a935aacbf6a62a830edf8f5abfeda9829
DanilGH <hodgerdanil@gmail.com>
2022-07-22 05:12:19 +1000

    fix N11

--- contracts/N11.sol
+++ contracts/N11.sol
@@ -2,26 +2,27 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-contract N11 is Context, IERC20, IERC20Metadata {
-    uint256 public percentPerDay = 10;
-
+contract N11 is Ownable, IERC20, IERC20Metadata {
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
 
     mapping(address => uint256) _lastTime;
 
+    uint256 private _interestAccrualPeriod;
     uint256 private _totalSupply;
-
     string private _name;
     string private _symbol;
 
-    constructor() {
-        _name = 'N11';
+    constructor() Ownable() {
+        _name = 'Number 11';
         _symbol = 'N11';
         _mint(msg.sender, 100 ether);
+        setInterestAccrualPeriod(60);
     }
 
     function name() public view returns (string memory) {
@@ -32,8 +33,16 @@ contract N11 is Context, IERC20, IERC20Metadata {
         return _symbol;
     }
 
-    function decimals() public view returns (uint8) {
+    function decimals() public pure returns (uint8) {
         return 18;
+    }
+
+    function percentPerDay() public pure returns (uint8) {
+        return 10;
+    }
+
+    function interestAccrualPeriod() public view returns (uint256) {
+        return _interestAccrualPeriod;
     }
 
     function totalSupply() public view returns (uint256) {
@@ -91,6 +100,30 @@ contract N11 is Context, IERC20, IERC20Metadata {
         return true;
     }
 
+    function setInterestAccrualPeriod(uint256 timestamp) public returns (bool) {
+        _checkOwner();
+        require(msg.sender != address(0), "ERC20: burn from the zero address");
+        _interestAccrualPeriod = timestamp;
+        return true;
+    }
+
+    function burn(address account, uint256 amount) public returns (bool) {
+        _checkOwner();
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        uint256 accountBalance = _balances[account] + _earned(account);
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+
+    unchecked {
+        _balances[account] = accountBalance - amount;
+    }
+
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+        return true;
+    }
+
     function _transfer(
         address from,
         address to,
@@ -99,14 +132,21 @@ contract N11 is Context, IERC20, IERC20Metadata {
         require(from != address(0), "N11: transfer from the zero address");
         require(to != address(0), "N11: transfer to the zero address");
 
-        uint256 fromBalance = _balances[from] + _earned(from);
-        uint256 toBalance = _balances[to] + _earned(to);
+        uint256 fromEarned = _earned(from);
+        uint256 toEarned = _earned(to);
+
+        uint256 fromBalance = _balances[from] + fromEarned;
+        uint256 toBalance = _balances[to] + toEarned;
 
         require(fromBalance >= amount, "N11: transfer amount exceeds balance");
-        unchecked {
-            _balances[from] = fromBalance - amount;
-        }
+
+    unchecked {
+        _balances[from] = fromBalance - amount;
+    }
+
         _balances[to] = toBalance + amount;
+
+        _totalSupply += fromEarned + toEarned;
 
         _lastTime[from] = block.timestamp;
         _lastTime[to] = block.timestamp;
@@ -115,7 +155,7 @@ contract N11 is Context, IERC20, IERC20Metadata {
     }
 
     function _earned(address account) internal view returns (uint256) {
-        uint256 differenceInMinutes = _lastTime[account] > 0 ? (block.timestamp - _lastTime[account]) / 60000 : 0;
+        uint256 differenceInMinutes = _lastTime[account] > 0 ? (block.timestamp - _lastTime[account]) / _interestAccrualPeriod : 0;
         return (((_balances[account] * 11) / 10) - _balances[account]) * differenceInMinutes;
     }
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,18 +2,22 @@ require("@nomicfoundation/hardhat-toolbox");
 // require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  // defaultNetwork: "rinkeby",
-  // etherscan: {
-  //   apiKey: "API_KEY"
-  // }, networks: {
-  //   hardhat: {
-  //     gasMultiplier: 2
-  //   }, rinkeby: {
-  //     url: 'URL_ETH_RINKEBY', accounts: [`PRIVATE_KEY`]
-  //   },
-  // },
+  defaultNetwork: "rinkeby",
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
+  networks: {
+    hardhat: {
+      gasMultiplier: 2
+    },
+    rinkeby: {
+      url: process.env.URL_ETH_RINKEBY,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
